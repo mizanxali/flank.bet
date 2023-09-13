0c3dd8d7304070e7ff8df86d66e935da4892397b
Mizan Ali <mizanalip786@gmail.com>
2023-09-13 07:48:48 +0530

    update landing page

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -6,16 +6,19 @@ import { FiPlusSquare, FiSettings } from "react-icons/fi";
 import { ImTicket } from "react-icons/im";
 import { BsTag } from "react-icons/bs";
 import { CgProfile } from "react-icons/cg";
+import Link from "next/link";
 
 function Sidebar() {
   return (
     <div className="mt-24 fixed top-0 left-0 w-60 flex flex-col gap-4 px-4">
-      <div className="flex gap-2 w-full items-center px-3 py-2 rounded-md cursor-pointer hover:text-light-2">
-        <span>
-          <BiSolidHomeSmile />
-        </span>
-        <span>Home</span>
-      </div>
+      <Link href="/">
+        <div className="flex gap-2 w-full items-center px-3 py-2 rounded-md cursor-pointer hover:text-light-2">
+          <span>
+            <BiSolidHomeSmile />
+          </span>
+          <span>Home</span>
+        </div>
+      </Link>
       <div className="flex gap-2 w-full items-center bg-dark-4 text-light-2 px-3 py-2 rounded-md cursor-pointer hover:text-light-2">
         <span>
           <HiOutlineSignal />
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,26 @@
 import Layout from "@/components/Layout";
 import { Chakra_Petch } from "next/font/google";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 const chakraPetch = Chakra_Petch({ subsets: ["latin"], weight: "700" });
 
 export default function Home() {
-  const { isConnected } = useAccount();
+  const [isDefinitelyConnected, setIsDefinitelyConnected] = useState(false);
+  const { address, isConnected } = useAccount();
+
+  useEffect(() => {
+    if (isConnected) {
+      setIsDefinitelyConnected(true);
+    } else {
+      setIsDefinitelyConnected(false);
+    }
+  }, [address]);
 
   return (
     <Layout>
       <div
-        className="flex-1 flex flex-col w-full h-full gap-4 items-center justify-center text-center"
+        className="flex-1 flex flex-col w-full h-full gap-5 items-center justify-center text-center"
         style={{
           backgroundImage: "url('/hero-bg.png')",
           backgroundRepeat: "no-repeat",
@@ -19,19 +29,26 @@ export default function Home() {
         }}
       >
         <h1 className={`${chakraPetch.className} text-5xl text-light-6`}>
-          Welcome to the new home of Esports MicroBetting
+          Welcome to the new home of Esports Micro-Betting
         </h1>
-        {/* {isConnected ? (
+        <p className={`${chakraPetch.className} text-2xl text-light-5`}>
+          Put your understanding of the game to the test by betting on
+          micro-events in esports matches using the power of crypto. Experience
+          seamless transactions, instant payouts, and enjoy a whole new level of
+          excitement and security.
+        </p>
+
+        {isDefinitelyConnected ? (
           <Link href="/games">
             <div className="bg-light-1 text-whites-3 font-semibold px-10 py-4 rounded-md">
               Browse Games
             </div>
           </Link>
         ) : (
-          <h6 className="text-2xl text-light-5">
-            Connect your wallet to get started
+          <h6 className="text-lg text-light-7">
+            Connect your wallet to get started.
           </h6>
-        )} */}
+        )}
       </div>
     </Layout>
   );
